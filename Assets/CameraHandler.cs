8686a5466c08c2478b221fe0087f85f5ed40da65
Titouan <molocai@molocai.com>
2017-02-02 10:41:51 +0100

    Basic click controller

---
+++ Assets/CameraHandler.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+
+namespace UbiSolarSystem
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        public enum CLICK_STATUS
+        {
+            HOVERING,
+            CLICKING,
+            DRAGGING,
+            RELEASING
+        }
+
+        #region Events
+        public delegate void ClickAction();
+        public static event ClickAction OnClickEvent;
+
+        public delegate void ReleaseAction();
+        public static event ReleaseAction OnReleaseEvent;
+
+        public delegate void DragAction();
+        public static event DragAction OnDragEvent;
+        #endregion
+
+        public CLICK_STATUS ClickStatus;
+        public Planet SelectedPlanet;
+
+        private void OnClickAction()
+        {
+            ClickStatus = CLICK_STATUS.CLICKING;
+            SelectedPlanet = GetPlanetAtPosition(Input.mousePosition);
+        }
+
+        private void OnReleaseAction()
+        {
+            ClickStatus = CLICK_STATUS.RELEASING;
+            SelectedPlanet = null;
+        }
+
+        private void OnDragAction()
+        {
+            ClickStatus = CLICK_STATUS.DRAGGING;
+            if (SelectedPlanet == null)
+            {
+                return;
+            }
+
+            SelectedPlanet.gameObject.transform.position = GetMousePositionInWorld();
+        }
+
+        void Start()
+        {
+            ClickStatus = CLICK_STATUS.HOVERING;
+
+            // Register all the events
+            OnClickEvent += OnClickAction;
+            OnReleaseEvent += OnReleaseAction;
+            OnDragEvent += OnDragAction;
+        }
+
+        void Update()
+        {
+            // Left click pressed
+            if (Input.GetMouseButtonDown(0) && ClickStatus == CLICK_STATUS.HOVERING)
+            {
+                if (OnClickEvent != null)
+                {
+                    OnClickEvent();
+                }
+            }
+
+            // Left click holded
+            if (Input.GetMouseButton(0) && (ClickStatus == CLICK_STATUS.DRAGGING || ClickStatus == CLICK_STATUS.CLICKING))
+            {
+                if (OnDragEvent != null)
+                {
+                    OnDragEvent();
+                }
+            }
+
+            // Left click released
+            if (Input.GetMouseButtonUp(0) && (ClickStatus == CLICK_STATUS.DRAGGING || ClickStatus == CLICK_STATUS.CLICKING))
+            {
+                if (OnReleaseEvent != null)
+                {
+                    OnReleaseEvent();
+                }
+            }
+
+            // Not doing anything
+            if (!Input.GetMouseButton(0) && ClickStatus == CLICK_STATUS.RELEASING)
+            {
+                ClickStatus = CLICK_STATUS.HOVERING;
+            }
+        }
+
+        private Planet GetPlanetAtPosition(Vector3 position)
+        {
+            RaycastHit hitInfo;
+            Ray rayhit = Camera.main.ScreenPointToRay(position);
+
+            Physics.Raycast(rayhit, out hitInfo, 1000, 1 << LayerMask.NameToLayer("Planet"), QueryTriggerInteraction.Ignore);
+
+            if (hitInfo.collider != null)
+            {
+                return hitInfo.collider.GetComponent<Planet>();
+            }
+
+            return null;
+        }
+
+        private Vector3 GetMousePositionInWorld()
+        {
+            RaycastHit hitInfo;
+            Ray rayhit = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(rayhit, out hitInfo, 1000, 1 << LayerMask.NameToLayer("Floor"), QueryTriggerInteraction.Ignore);
+
+            if (hitInfo.collider != null)
+            {
+                return hitInfo.point;
+            }
+
+            return Vector3.zero;
+        }
+
+        //private void OnDrawGizmos()
+        //{
+        //    Gizmos.color = Color.red;
+
+        //    RaycastHit hitInfo;
+        //    Ray rayhit = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    Physics.Raycast(rayhit, out hitInfo, 1000, 1 << LayerMask.NameToLayer("Floor"), QueryTriggerInteraction.Ignore);
+
+        //    if (hitInfo.collider != null)
+        //    {
+        //        Gizmos.DrawWireSphere(hitInfo.point, 1f);
+        //    }
+        //}
+    }
+}
---
+++ Assets/Planet.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace UbiSolarSystem
+{
+    public class Planet : MonoBehaviour
+    {
+        public float Mass = 10f;
+        public bool AffectOtherPlanets = true;
+
+        private List<Planet> PlanetsAffectingMe;
+
+        // Use this for initialization
+        void Start()
+        {
+            PlanetsAffectingMe = new List<Planet>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach(Planet planet in PlanetsAffectingMe)
+            {
+                float gravityPull = (planet.Mass * this.Mass) / Mathf.Abs(Vector3.Distance(planet.transform.position, transform.position));
+                Vector3 unitVector = Vector3.Normalize(planet.transform.position - transform.position);
+                //Vector3 gravityForce = gravityPull * unitVector;
+
+                Vector3 newPos = transform.position + unitVector * gravityPull;
+
+                transform.position = newPos * Time.deltaTime;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Planet otherPlanet = other.GetComponent<Planet>();
+            if (otherPlanet)
+            {
+                PlanetsAffectingMe.Add(otherPlanet);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Planet otherPlanet = other.GetComponent<Planet>();
+            if (otherPlanet)
+            {
+                PlanetsAffectingMe.Remove(otherPlanet);
+            }
+        }
+    }
+}
